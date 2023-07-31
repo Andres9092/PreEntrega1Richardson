11e08433166faec8140e7efad24899ea58f004ea
Andres9092 <richardsonandres1@gmail.com>
2023-07-31 21:53:13 +1000

    avance header 31/7 noche

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,46 +6,76 @@ import CartWidget from './CartWidget';
 
 function NavBar() { 
     return (  
-
-        
-      
-        <header className="NavHeaderPhone">   {/* etiqueta unica que contiene la aplicacion completa */}
+     
+        <header className="Header">   {/* etiqueta unica que contiene la aplicacion completa */}
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/> {/* Importar estilos para el logo burger menu */}
   
-            <div className="burger-logo-Phone">   
+            <div className="NavHeader">
+                <div className="burger-logo-Phone">   
 
-                <div className="burger-menu">
-    
-                    <button type="submit" className="botonBurger"><i className="fas fa-bars fa-2x"></i></button>
+                    <div className="burger-menu">
         
-                </div>  
-        
-               
-                <img className="logoEnjDeco" src={imagenlogo} alt="ImagenmLogo Enjoying Deco"/>
-            </div>
+                        <button type="submit" className="botonBurger"><i className="fas fa-bars fa-2x"></i></button>
+            
+                    </div>  
+            
+                
+                    <img className="logoEnjDeco" src={imagenlogo} alt="ImagenmLogo Enjoying Deco"/>
+                </div>
+
+                <div>
+                    <div className="barraBuscadora">
+                            
+
+                            <input type="text" name="buscar" className="textoBuscarEnBarra" placeholder="Buscar..."/>
+                                
+                            <button className="botonSearch-Desktop" type="submit"><i className="fa-solid fa-magnifying-glass botonSearchDektop"></i></button>
+
+
+                    </div>
 
                 
-            <div  className="linksyCarritoPhone">   
-                <div className="linksPhone">
+                </div>
+
                     
-                    <ul className = "logosMargenDerechoPhone">
-
+                <div  className="linksyCarritoPhone">   
+                    <div className="linksPhone">
                         
-                        <li><a className="iconoBuscarMargenDerecho" href="aaaaa.html"><i className="fa-solid fa-magnifying-glass"></i></a></li>
-                        <li><a className="logo-wapp-desktop" href="aaaaa.html"><i className="fa-brands fa-whatsapp"></i></a></li>
-                    </ul>
+                        <ul className = "logosMargenDerechoPhone">
 
-                </div>
                             
-            
-                <div>
-                    <CartWidget>          {/* Llamo a C Hijo 'CartWidget' */}
+                            <li><a className="iconoBuscarMargenDerecho" href="aaaaa.html"><i className="fa-solid fa-magnifying-glass"></i></a></li>
+                            <li><a className="logo-wapp-desktop" href="aaaaa.html"><i className="fa-brands fa-whatsapp"></i></a></li>
+                            <li><a className="logo-wapp-desktop" href="aaaaa.html"><i class="fa-regular fa-heart"></i></a></li>
+                            <li><a className="logo-wapp-desktop" href="aaaaa.html"><i class="fa-solid fa-user"></i></a></li>
+                        </ul>
 
-                    </CartWidget>
+                    </div>
+                                                    
+                
+                    <div>
+                        <CartWidget>          {/* Llamo a C Hijo 'CartWidget' */}
 
+                        </CartWidget>
+
+                    </div>
                 </div>
+            
+            </div>
 
+            <div className="NavInferior">
+   
+                <ul className="leftBottomHeader">
+
+                    <li ><a className="Inicio" href="/">Inicio</a></li>
+                    <li ><a className="Ofertas" href="Ofertas.html">Ofertas</a></li>
+                    <li ><a className="Blog" href="Blog.html">Blog</a></li>
+                    <li ><a className="Sucursales" href="Sucursales.html">Sucursales</a></li>
+                    <li ><a className="Contacto" href="Contacto.html">Contacto</a></li>
+                
+                </ul>                                   
+                
             </div>
 
     </header >
