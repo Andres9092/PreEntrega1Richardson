576f2b9b9c8ecdc4a3f3974a471a3745e169155a
Andres9092 <richardsonandres1@gmail.com>
2023-08-09 20:33:02 +1000

    fin header con arreglos 9-8 tarde

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,9 +46,9 @@ function NavBar() {
 
                             
                             <li><a className="iconoBuscarMargenDerecho" href="aaaaa.html"><i className="fa-solid fa-magnifying-glass"></i></a></li>
-                            <li><a className="logo-wapp-desktop" href="aaaaa.html"><i className="fa-brands fa-whatsapp"></i></a></li>
-                            <li><a className="logo-favoritos" href="aaaaa.html"><i class="fa-regular fa-heart"></i></a></li>
-                            <li><a className="logo-user" href="aaaaa.html"><i class="fa-regular fa-user"></i></a></li>
+                            <li><a className="logo-wapp-desktop" href="aaaaa.html"><i className="fa-solid fa-brands fa-whatsapp"></i></a></li>
+                            <li><a className="logo-favoritos" href="aaaaa.html"><i class="fa-solid fa-regular fa-heart"></i></a></li>
+                            <li><a className="logo-user" href="aaaaa.html"><i class=" fa-solid fa-regular fa-user"></i></a></li>
                         </ul>
 
                     </div>
@@ -69,12 +69,35 @@ function NavBar() {
                 <ul className="leftBottomHeader">
                 
                     
-                    <li ><a className="Productos" href="/">Ramos</a></li>
-                    <li ><a className="Categorias" href="/">Categorias</a></li>
-                    <li ><a className="Ofertas" href="Ofertas.html">Ofertas!</a></li>
-                    <li ><a className="Blog" href="Blog.html">Blog</a></li>
-                    <li ><a className="Sucursales" href="Sucursales.html">Sucursales</a></li>
-                    <li ><a className="Contacto" href="Contacto.html">Contacto</a></li>                
+                    <div className = "icono-palabra">
+                    <li ><i class="fa-brands fa-pagelines"></i></li>
+                    <li ><a className="iconos-pala" href="/">Ramos</a></li>
+                    </div>
+
+                    <div className = "icono-palabra">
+                    <li ><i class="fa-solid fa-list"></i></li>
+                    <li ><a className="iconos-pala" href="/">Categorias</a></li>
+                    </div>
+
+                    <div className = "icono-palabra">
+                    <li ><i class="fa-solid fa-burst"></i></li>
+                    <li ><a className="iconos-pala" href="Ofertas.html">Ofertas!</a></li>
+                    </div>
+
+                    <div className = "icono-palabra">
+                    <li ><i class="fa-solid fa-book"></i></li>
+                    <li ><a className="iconos-pala" href="Blog.html">Blog</a></li>
+                    </div>
+
+                    <div className = "icono-palabra">
+                    <li ><i class="fa-solid fa-house"></i></li>
+                    <li ><a className="iconos-pala" href="Sucursales.html">Sucursales</a></li>
+                    </div>
+
+                    <div className = "icono-palabra">
+                    <li ><i class="fa-solid fa-phone"></i></li>
+                    <li ><a className="iconos-pala" href="Contacto.html">Contacto</a></li>  
+                    </div>              
                 </ul>    
 
                                            
