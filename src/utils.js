a617344a121fcb4104a6b1c7e02fb65d4c9a2b0a
Andres9092 <richardsonandres1@gmail.com>
2023-08-29 00:58:18 +1000

    utils.js

---
+++ src/utils.js
@@ -0,0 +1,79 @@
+export const productosHarcodeados = [
+    {
+    id: 1,
+    nombre: 'Taormina',
+    descripcion: 'A',
+    tamanio: 'M',
+    precio: 3500,
+    foto:ramo1,
+    eucalipto: 'si'
+
+  },
+  {
+    id: 2,
+    nombre: 'Bellagio',
+    descripcion: 'B',
+    tamanio: 'M',
+    precio: 6000,
+    foto:ramo2,
+    eucalipto: 'si'
+
+  },
+  {
+    id: 3,
+    nombre: 'Milano',
+    descripcion: 'C',
+    tamanio: 'L',
+    precio: 2000,
+    foto:ramo3,
+    eucalipto: 'no'
+
+  },
+  {
+    id: 4,
+    nombre: 'Eucalipto',
+    descripcion: 'D',
+    tamanio: 'L',
+    precio: 4300,
+    foto:ramo4,
+    eucalipto: 'no'
+
+  },
+  {
+    id: 5,
+    nombre: 'Eucalipto',
+    descripcion: 'D',
+    tamanio: 'L',
+    precio: 3600,
+    foto:ramo5,
+    eucalipto: 'si'
+
+  },{
+    id: 6,
+    nombre: 'Eucalipto',
+    descripcion: 'D',
+    tamanio: 'M',
+    precio: 5200,
+    foto:ramo6,
+    eucalipto: 'no'
+
+  },{
+    id: 7,
+    nombre: 'Eucalipto',
+    descripcion: 'D',
+    tamanio: 'M',
+    precio: 2600,
+    foto:ramo7,
+    eucalipto: 'si'
+
+  },
+  {
+    id: 8,
+    nombre: 'Eucalipto',
+    descripcion: 'D',
+    tamanio: 'M',
+    precio: 4500,
+    foto:ramo8,
+    eucalipto: 'no'
+
+  }] 
