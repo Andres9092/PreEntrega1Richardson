f22cb73f7b64a2fc52712bf6cf487e8d397959c5
Andres Richardson <andres.richardson@axiomps.com.au>
2023-08-31 09:37:33 +1000

    condicional logo carrito

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -5,16 +5,27 @@ import {useContext} from 'react';
 import {contexto} from './CustomProvider';
 
 
-function CartWidget() { 
+export function CartWidget({user = true}) { //Si no estoy logueado como 'user' no se muestra el logo de Carrito.
 
   const valorDelContexto = useContext(contexto)
-    return (  
+    
+  if (! user){
+    console.log('Ud no tiene credenciales para entrar al carrito de compras')
+  
+    return(
+      <h1></h1>
+    )
+    
+  }
+  
+  
+  return (  
       
       <div className="CartW">   
         <ul className = "logoCart">        
             
             <li><Link className="iconoCarrito" to = "/carrito"><i class="fa-solid fa-cart-plus"></i></Link></li>
-            <p className="numeroCart"> {valorDelContexto.cantidadTotal} </p>
+            <p className="numeroCart"> {valorDelContexto.cantidadTotal} </p>    {/* valorDelContexto es la variable prop global, proveniente de CustomProvider. */}
         </ul>
          
   
