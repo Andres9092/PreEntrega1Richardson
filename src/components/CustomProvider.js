377c538a6f175e63a26dba519d9efc2204ed9b8c
Andres9092 <richardsonandres1@gmail.com>
2023-09-06 21:22:28 +1000

    detalle

--- src/components/CustomProvider.js
+++ src/components/CustomProvider.js
@@ -11,21 +11,15 @@ const Provider = contexto.Provider          // 4) Sacar 'Provider' de adentro de
 const CustomProvider = (props) => {         // 1) Creo ESTE archivo con componente CustomProvider.
                                             // 2) Colocar al componente 'Provider' donde convenga, englobando a los C hijos que se requiera (En este caso dentro de App, englobando a 'NavBar', 'Main' y 'Footer')
   const [cantidadCart, setcantidadCart] = useState([])
-  console.log("cart :", cantidadCart)
-  //const [cantidad, setCantidad] = useState(4)
+  console.log("cantidadCart :", cantidadCart)
 
-  const valorDelContexto = {                // valorDelContexto -> asigno objeto con pares de key,values que luego podran ser utilizados en el resto de los C hijos.
+  
 
-    carrito: [],
-    montoTotal: 0,
-    cantidadTotal: 2,
-    user: 'sd',
-
-}
 
     return (  
       
-      <Provider value = {valorDelContexto}> {/* 5) Se usa el 'Provider' dentro del return. Le provee a la parte de la aplicacion que englobe el CustomProvider, del valor del Contexto, a traves de 'value'.*/}
+
+      <Provider value = {{cantidadCart}}> {/* 5) Se usa el 'Provider' dentro del return. Le provee a la parte de la aplicacion que englobe el CustomProvider, del valor del Contexto, a traves de 'value'.*/}
           
           {props.children}     {/* Children -> C hijos que engloba 'CustomProvider' y a partir de los cuales se puede acceder por 'props' a 'value' globalmente. */}
           
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -9,124 +9,24 @@ import {getDoc, collection, doc} from 'firebase/firestore';
 import {useEffect, useState} from 'react';
 import BarsLoader from 'react-loaders-kit/lib/bars/BarsLoader'
 
-{/*
-const products =  [
-    {
-    id: 1,
-    nombre: 'Taormina',
-    descripcion: 'A',
-    tamanio: 'M',
-    precio: 3500,
-    foto:ramo1,
-    eucalipto: 'si',
-    stock: 10
-
-  },
-  {
-    id: 2,
-    nombre: 'Bellagio',
-    descripcion: 'B',
-    tamanio: 'M',
-    precio: 6000,
-    foto:ramo2,
-    eucalipto: 'si',
-    stock: 2
-
-  },
-  {
-    id: 3,
-    nombre: 'Milano',
-    descripcion: 'C',
-    tamanio: 'L',
-    precio: 2000,
-    foto:ramo3,
-    eucalipto: 'no', 
-    stock: 1
-
-  },
-  {
-    id: 4,
-    nombre: 'Eucalipto',
-    descripcion: 'D',
-    tamanio: 'L',
-    precio: 4300,
-    foto:ramo4,
-    eucalipto: 'no',
-    stock: 10
-
-  },
-  {
-    id: 5,
-    nombre: 'Eucalipto',
-    descripcion: 'D',
-    tamanio: 'L',
-    precio: 3600,
-    foto:ramo5,
-    eucalipto: 'si',
-    stock: 8
-
-  },{
-    id: 6,
-    nombre: 'Eucalipto',
-    descripcion: 'D',
-    tamanio: 'M',
-    precio: 5200,
-    foto:ramo6,
-    eucalipto: 'no',
-    stock: 0
-
-  },{
-    id: 7,
-    nombre: 'Eucalipto',
-    descripcion: 'D',
-    tamanio: 'M',
-    precio: 2600,
-    foto:ramo7,
-    eucalipto: 'si',
-    stock: 0
-
-  },
-  {
-    id: 8,
-    nombre: 'Eucalipto',
-    descripcion: 'D',
-    tamanio: 'M',
-    precio: 4500,
-    foto:ramo8,
-    eucalipto: 'no',
-    stock: 4
-
-  }]*/};
-
 
 function ItemDetail({contador}) { //StateUpLifting. Le paso al C padre 'ItemDetail' la prop del C hijo Contador.
 
     const {id} = useParams()   
     console.log('id :', id)   //id : 1cVNWdY0BDnjelTnoAfL -> el value es el pasado desde por 'products' desde ItemListContainer.
 
-
-  //const resultado = useParams()   // useParams() ES UN OBJETO. Captura el objeto obtenido por la ruta. VALORES POSIBLES PARA EL OBJETO useParams.-> {} si se pasa la ruta /, {id: M} si se pasa la ruta /categoria/M, {id: L} si se pasa la ruta /categoria/L}
-  //console.log('resultado: ', resultado)
-
-  //const id = resultado.id //Accedo a =l value de la key.
-  //console.log('id: ',id)   
-    
     const [product, setProduct] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState("")
-
     const [cantidad, setCantidad] = useState(0)
     
-    
-
     const loaderProps = {
       loading,
       size: 40,
       duration: 1,
       colors: ['#c99d0b', '#cfab35']}
   
-  {/*Hook para ejecutar la funcion 1 vez unicamente, que cambia el estado inicial vacio de 'products', por medio de setProduct con la data array traida de la variable creada dentro de la funcion. */}
-   
+  
     useEffect( () => {
       
       const productCollection = collection(db, 'productos')
@@ -153,13 +53,6 @@ function ItemDetail({contador}) { //StateUpLifting. Le paso al C padre 'ItemDeta
           setTimeout(() => {
             setLoading(false)
           },1000)
-  
-                   
-          //const productsDetalle = products.find((prod) => prod.id === Number(id)); // barro la data y me quedo con el objeto cuyo id == id capturado por el useParams
-                                                                                      
-          //console.log('producto :', productsDetalle)
-          
-        {/*  getProducts (products)  */}
     
   }, []);
 
@@ -167,19 +60,18 @@ function ItemDetail({contador}) { //StateUpLifting. Le paso al C padre 'ItemDeta
 
         setCantidad(cantidadConfirmada)  // setea nuevo valor a 'cantidad'
         console.log("La cantidad confirmada es: ", cantidadConfirmada )
-       const item ={
-        id
+        
+        
+        const item ={                             //creo item con caracteristicas del producto y la cantidad confirmada del mismo.
+          product, cantidadConfirmada
+    
        }
+       console.log("item: ", item )
+       console.log("item.product.precio: ", item.product.precio)
       }
-   
-
-  {/* id es la 'clave' del 'value' a capturar por URL del objeto useParams, cuando apreto el boton 'ver detalle' que esta asociado a la ruta "/productos/:id"*/}
-
-    //if (loading) return  <p>Cargando...</p>
-
+ 
     return (  
         
-      
     <div className="ItemDet">  
    
             
@@ -230,7 +122,7 @@ function ItemDetail({contador}) { //StateUpLifting. Le paso al C padre 'ItemDeta
                 <div>
                   {  
                     cantidad > 0 ? (  // Inicialmente el link no se ve ya que 'cantidad = 0' y se muestra el boton del Comp COntador 'Agregar al carrito'. Al re setearse el valor de 'cantidad', se muestra el link 'Terminar compra' y desaparece el boton 'Agregar al carrito'
-                    <Link to ="/carrito" className="botonTerminarCompra"><i class="fa-solid fa-money-check-dollar"></i> Terminar compra</Link>
+                    <Link to ="/carrito" className="botonTerminarCompra"><i class="fa-solid fa-money-check-dollar"></i> Finalizar compra</Link>
                     ) : (
                         <Contador  nombrePropHandleCallback = {handleCallback} stock = {product.stock} initial ={1}/>  /* Le paso al C hijo Contador la prop  'nombrePropHandleCallback' cuyo contenido es la funcion 'handleCallback'  */
                     )
@@ -312,7 +204,7 @@ function ItemDetail({contador}) { //StateUpLifting. Le paso al C padre 'ItemDeta
                                 <div>
                                   {  
                                     cantidad > 0 ? (
-                                          <Link to ="/carrito" className="botonTerminarCompra"><i class="fa-solid fa-money-check-dollar"></i> Terminar compra</Link>  /* Le paso al C hijo Contador la prop  'nombrePropHandleCallback' cuyo contenido es la funcion 'handleCallback'  */
+                                          <Link to ="/carrito" className="botonTerminarCompra"><i class="fa-solid fa-money-check-dollar"></i> Finalizar compra</Link>  /* Le paso al C hijo Contador la prop  'nombrePropHandleCallback' cuyo contenido es la funcion 'handleCallback'  */
                                     ) : (
                                         <Contador  nombrePropHandleCallback = {handleCallback} stock = {product.stock} initial ={1}/>  
                                     )
